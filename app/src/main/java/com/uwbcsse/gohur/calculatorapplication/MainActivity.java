94e7a1de167c2dda62a55fc4ed4d2d18d52b7ad3
gohurali <gohurali@live.com>
2018-07-06 00:42:12 -0700

    Created Button Fields

--- app/src/main/java/com/uwbcsse/gohur/calculatorapplication/MainActivity.java
+++ app/src/main/java/com/uwbcsse/gohur/calculatorapplication/MainActivity.java
@@ -2,8 +2,34 @@ package com.uwbcsse.gohur.calculatorapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //------------------Button Fields------------------------------
+    private Button zero;
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+
+    //Operator Buttons;
+    private Button add;
+    private Button subtract;
+    private Button multiply;
+    private Button divide;
+    private Button equals;
+
+    //Result
+    private TextView info;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
